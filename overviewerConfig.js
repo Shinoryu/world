7dfd8aacb6c5e26dba30d401d0142906c5b92aa4
Alex Chan <shinoryu10@gmail.com>
2018-07-15 19:44:58 -0400

    update

--- overviewerConfig.js
+++ overviewerConfig.js
@@ -10,18 +10,18 @@ var overviewerConfig = {
                 -96
             ], 
             "isOverlay": false, 
-            "last_rendertime": 1531696484, 
+            "last_rendertime": 1531697756, 
             "name": "Normal render of world", 
             "poititle": "Markers", 
             "north_direction": 0, 
             "minZoom": 0, 
             "bgcolor": "#1a1a1a", 
-            "zoomLevels": 8, 
+            "zoomLevels": 9, 
             "base": "", 
             "imgextension": "png", 
             "defaultZoom": 1, 
             "world": "world", 
-            "maxZoom": 8, 
+            "maxZoom": 9, 
             "path": "normal", 
             "showlocationmarker": true
         }
@@ -66,6 +66,6 @@ var overviewerConfig = {
             67, 
             94
         ], 
-        "cacheTag": "1531696656"
+        "cacheTag": "1531697992"
     }
 };
