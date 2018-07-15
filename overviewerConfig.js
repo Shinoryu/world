b0cf65a55ec44eae76ef110ef35b9ab46881cdfe
Alex Chan <shinoryu10@gmail.com>
2018-07-15 16:09:13 -0400

    update

--- overviewerConfig.js
+++ overviewerConfig.js
@@ -10,7 +10,7 @@ var overviewerConfig = {
                 -96
             ], 
             "isOverlay": false, 
-            "last_rendertime": 1531678189, 
+            "last_rendertime": 1531685114, 
             "name": "Overviewer Render (lighting)", 
             "poititle": "Markers", 
             "north_direction": 0, 
@@ -66,6 +66,6 @@ var overviewerConfig = {
             67, 
             94
         ], 
-        "cacheTag": "1531678290"
+        "cacheTag": "1531685192"
     }
 };
